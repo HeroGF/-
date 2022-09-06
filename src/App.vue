de5de7fb9b626361ca7e409d7f87d9a38f935e1c
Seven <1319129926@qq.com>
2022-09-06 16:13:36 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    <span class="toutiao toutiao-gengduo">更多</span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">登录</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.box {
+  width: 694px;
+  height: 88px;
+  line-height: 88px;
+  font-size: 30px;
+  text-align: center;
+  background-color: deeppink;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入字体图标样式
+import '@/assets/fonts/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入flexible
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
